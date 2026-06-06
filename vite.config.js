0cd616ce184920896c2a93f07b4f33939cf071f3
AtefaKhan <149286421+AtefaKhan@users.noreply.github.com>
2026-06-06 19:48:20 +0530

    packages added

---
+++ vite.config.js
@@ -0,0 +1,58 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  base: "/Optimus-Ecomm/",
+
+  plugins: [
+    react({
+      babel: {
+        plugins: ["@babel/plugin-syntax-import-meta"],
+      },
+    }),
+    tailwindcss(),
+  ],
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "react-vendor";
+            if (id.includes("framer-motion")) return "animation-vendor";
+            if (id.includes("lucide-react") || id.includes("react-icons"))
+              return "icons-vendor";
+
+            return "vendor";
+          }
+        },
+      },
+    },
+
+    chunkSizeWarningLimit: 1000,
+
+    minify: "terser",
+
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "src/styles/variables";`,
+      },
+    },
+  },
+
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    middlewareMode: false,
+  },
+});
